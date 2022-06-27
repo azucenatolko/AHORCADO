30ace1c5a3eb2e4e26ac30ef586641c96e5f3b20
azucenatolko <acervantes@tolkogroup.com>
2022-06-27 18:38:17 -0500

    limpio
    limpio

--- javascript.js
+++ javascript.js
@@ -22,6 +22,10 @@ const palabras = [
         {
             palabra: 'tarjeta',
             pista: 'es de plástico',
+        },
+        {
+            palabra: 'otro',
+            pista: 'es otro',
         }
 ];
 let palabra = [];
@@ -36,7 +40,6 @@ function nuevoJuego(){
     document.querySelector('#ahorcado').style.backgroundPosition= '0px 0';
     document.querySelector('#pista').innerHTML ='Pista: ' + palabra.pista;
 }
-
 
 function buscaLetra(a){
     const letra = a;
@@ -59,152 +62,9 @@ function buscaLetra(a){
     else{
         if (palabraGuiones.indexOf('_')<0){
             //alert("GANASTE, la palabra es: " + palabra.palabra );
-           
         }
     }
    
 };
 
 
-
-
-
-
-//const x = palabras2.map(function(palabra) {
-//    return alert(palabra.pista);
-//});
-
-
-
-
-//AZU V.1
-/*
-
-String.prototype.replaceAt=function(index, character){ 
-    return this.substr(0, index) + character + this.substr(index+character.length);}
-const palabras = ['dinero','ahorro','banco','credito','plazos','tarjeta'];
-let palabra = "";
-let palabraGuiones = "";
-nuevoJuego();
-
-const palabras2 = [
-        {
-            palabra: 'dinero',
-            pista: 'Es verde',
-        },
-        {
-            palabra: 'banco',
-            pista: 'Guarda el dinero',
-        },
-        {
-            palabra: 'crédito',
-            pista: 'duele pagarlo',
-        },
-        {
-            palabra: 'plazos',
-            pista: 'lapso de tiempo',
-        },
-        {
-            palabra: 'tarjeta',
-            pista: 'es de plástico',
-        }
-];
-
-//let palabra2 = palabras2.palabra[Math.floor(Math.random()*palabras2.palabra.length)];
-//alert(palabra2);
-let palabra2 = [];
-palabra2 = palabras2[Math.floor(Math.random()*palabras2.length)];
-alert(palabra2.palabra);
-alert(palabra2.pista);
-
-//const x = palabras2.map(function(palabra) {
-//    return alert(palabra.pista);
-//});
-
-function nuevoJuego(){
-    palabra = palabras[Math.floor(Math.random()*palabras.length)];
-    palabraGuiones = palabra.replace(/./g,"_ ");
-    contadorfallas = 0;
-    alert(palabra + "-" + palabraGuiones);
-    document.querySelector('#guiones').innerHTML = palabraGuiones;
-    document.querySelector('#ahorcado').style.backgroundPosition= '0px 0';
-}
-
-
-function buscaLetra(a){
-    const letra = a;
-    let falla = true;
-    for(const i in palabra){
-        if (letra == palabra[i]){
-            palabraGuiones = palabraGuiones.replaceAt(i*2, letra);
-            falla = false;
-         }
-    }
-    if(falla){
-        contadorfallas++;
-        document.querySelector('#ahorcado').style.backgroundPosition= -(400*contadorfallas) + 'px 0';
-        if (contadorfallas == 6){
-            alert("perdiste");
-        }
-    }
-    else{
-        if (palabraGuiones.indexOf('_')<0){
-            alert("ganaste");
-        }
-    }
-    document.querySelector('#guiones').innerHTML = palabraGuiones;
-};
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//JUEGO DEL AHORCADO EJEMPLO
-
-
-
-/*
-document.querySelector('#calcular').addEventListener('click',()=>
-{
-    const letra = document.querySelector('#letra').value;
-    let falla = true;
-    for(const i in palabra){
-        if (letra == palabra[i]){
-            palabraGuiones = palabraGuiones.replaceAt(i*2, letra);
-            falla = false;
-
-         }
-    }
-
-    if(falla){
-        alert("fallaste");
-        contadorfallas++;
-        if (contadorfallas == 5){
-            alert("perdiste");
-        }
-    }
-    else{
-        if (palabraGuiones.indexOf('_')<0){
-            alert("ganaste");
-        }
-    }
-    document.querySelector('#guiones').innerHTML = palabraGuiones;
-    document.querySelector('#letra').value = '';
-
-});*/
